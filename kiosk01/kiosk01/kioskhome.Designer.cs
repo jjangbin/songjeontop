e14de0616293a7e24285d3130e0e896141ec6345
jjangbin <75109836+jjangbin@users.noreply.github.com>
2020-12-20 11:56:58 +0900

    이제된다 얘두라!!!!!! 화이팅

--- kiosk01/kiosk01/kioskhome.Designer.cs
+++ kiosk01/kiosk01/kioskhome.Designer.cs
@@ -64,10 +64,10 @@ namespace kiosk01
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(865, 22);
+            this.button1.Location = new System.Drawing.Point(989, 28);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(45, 34);
+            this.button1.Size = new System.Drawing.Size(51, 42);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -78,10 +78,10 @@ namespace kiosk01
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(910, 22);
+            this.button2.Location = new System.Drawing.Point(1040, 28);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(45, 34);
+            this.button2.Size = new System.Drawing.Size(51, 42);
             this.button2.TabIndex = 0;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -89,10 +89,10 @@ namespace kiosk01
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(10, 338);
+            this.pictureBox1.Location = new System.Drawing.Point(11, 422);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(288, 174);
+            this.pictureBox1.Size = new System.Drawing.Size(329, 218);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -102,9 +102,9 @@ namespace kiosk01
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.LightSteelBlue;
-            this.label1.Location = new System.Drawing.Point(346, 423);
+            this.label1.Location = new System.Drawing.Point(395, 529);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(534, 79);
+            this.label1.Size = new System.Drawing.Size(670, 98);
             this.label1.TabIndex = 4;
             this.label1.Text = "화면을 터치하세요.";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -116,7 +116,7 @@ namespace kiosk01
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(954, 20);
+            this.panel1.Size = new System.Drawing.Size(1090, 25);
             this.panel1.TabIndex = 5;
             // 
             // panel2
@@ -124,10 +124,10 @@ namespace kiosk01
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(49)))), ((int)(((byte)(96)))));
             this.panel2.Controls.Add(this.label2);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 516);
+            this.panel2.Location = new System.Drawing.Point(0, 645);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(954, 20);
+            this.panel2.Size = new System.Drawing.Size(1090, 25);
             this.panel2.TabIndex = 6;
             // 
             // label2
@@ -135,19 +135,19 @@ namespace kiosk01
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(740, 0);
+            this.label2.Location = new System.Drawing.Point(846, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(214, 21);
+            this.label2.Size = new System.Drawing.Size(248, 23);
             this.label2.TabIndex = 0;
             this.label2.Text = "2020년 12월 19일 AM 10:37";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(-1, 19);
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 24);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(186, 54);
+            this.pictureBox2.Size = new System.Drawing.Size(213, 68);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
@@ -158,20 +158,20 @@ namespace kiosk01
             this.home.FlatAppearance.BorderSize = 0;
             this.home.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.home.Image = ((System.Drawing.Image)(resources.GetObject("home.Image")));
-            this.home.Location = new System.Drawing.Point(821, 22);
+            this.home.Location = new System.Drawing.Point(938, 28);
             this.home.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.home.Name = "home";
-            this.home.Size = new System.Drawing.Size(45, 34);
+            this.home.Size = new System.Drawing.Size(51, 42);
             this.home.TabIndex = 0;
             this.home.UseVisualStyleBackColor = true;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(10, 99);
+            this.pictureBox3.Location = new System.Drawing.Point(11, 124);
             this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(288, 121);
+            this.pictureBox3.Size = new System.Drawing.Size(329, 151);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 1;
             this.pictureBox3.TabStop = false;
@@ -179,10 +179,10 @@ namespace kiosk01
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(10, 222);
+            this.pictureBox4.Location = new System.Drawing.Point(11, 278);
             this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(288, 113);
+            this.pictureBox4.Size = new System.Drawing.Size(329, 141);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 1;
             this.pictureBox4.TabStop = false;
@@ -192,19 +192,19 @@ namespace kiosk01
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(77)))), ((int)(((byte)(21)))));
-            this.label3.Location = new System.Drawing.Point(3, 38);
+            this.label3.Location = new System.Drawing.Point(3, 48);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(313, 77);
+            this.label3.Size = new System.Drawing.Size(385, 93);
             this.label3.TabIndex = 8;
             this.label3.Text = "돈까스정식";
             // 
             // pictureBox5
             // 
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(17, 114);
+            this.pictureBox5.Location = new System.Drawing.Point(19, 142);
             this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(289, 209);
+            this.pictureBox5.Size = new System.Drawing.Size(330, 261);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 9;
             this.pictureBox5.TabStop = false;
@@ -217,10 +217,10 @@ namespace kiosk01
             this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.pictureBox5);
-            this.panel3.Location = new System.Drawing.Point(332, 82);
+            this.panel3.Location = new System.Drawing.Point(379, 102);
             this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(611, 339);
+            this.panel3.Size = new System.Drawing.Size(698, 424);
             this.panel3.TabIndex = 10;
             // 
             // button3
@@ -231,10 +231,10 @@ namespace kiosk01
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(311, 278);
+            this.button3.Location = new System.Drawing.Point(355, 348);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(298, 46);
+            this.button3.Size = new System.Drawing.Size(341, 58);
             this.button3.TabIndex = 12;
             this.button3.Text = "주문하기";
             this.button3.UseVisualStyleBackColor = false;
@@ -243,9 +243,9 @@ namespace kiosk01
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(311, 132);
+            this.label6.Location = new System.Drawing.Point(355, 165);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(263, 102);
+            this.label6.Size = new System.Drawing.Size(329, 120);
             this.label6.TabIndex = 11;
             this.label6.Text = "안심하고 먹을 수 있는 국내산 등심을 사용한\r\n등심돈까스정식이 새롭게 출시되었습니다.\r\n겉은 바삭! 속은 부드럽고 촉촉한 등심돈가스를\r\n단돈 70" +
     "00원에 즐겨보세요!\r\n첫 출시 기념으로 돈까스정식을 주문하신 분들께\r\n초밥 2pc를 서비스로 제공해드립니다.";
@@ -255,9 +255,9 @@ namespace kiosk01
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Gray;
-            this.label5.Location = new System.Drawing.Point(312, 243);
+            this.label5.Location = new System.Drawing.Point(357, 304);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(248, 30);
+            this.label5.Size = new System.Drawing.Size(264, 34);
             this.label5.TabIndex = 11;
             this.label5.Text = "돼지고기 : 국내산 \r\n쌀 : 국내산 / 배추 : 국내산 / 고춧가루 : 중국산\r\n";
             // 
@@ -266,27 +266,27 @@ namespace kiosk01
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Firebrick;
-            this.label4.Location = new System.Drawing.Point(10, 18);
+            this.label4.Location = new System.Drawing.Point(11, 22);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(177, 32);
+            this.label4.Size = new System.Drawing.Size(222, 39);
             this.label4.TabIndex = 10;
             this.label4.Text = "오늘의 추천메뉴";
             // 
             // form_menulist1
             // 
             this.form_menulist1.BackColor = System.Drawing.Color.White;
-            this.form_menulist1.Location = new System.Drawing.Point(-1, 19);
+            this.form_menulist1.Location = new System.Drawing.Point(4, 90);
             this.form_menulist1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.form_menulist1.Name = "form_menulist1";
-            this.form_menulist1.Size = new System.Drawing.Size(955, 495);
+            this.form_menulist1.Size = new System.Drawing.Size(1090, 550);
             this.form_menulist1.TabIndex = 11;
             // 
             // kioskhome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(954, 536);
+            this.ClientSize = new System.Drawing.Size(1090, 670);
             this.Controls.Add(this.form_menulist1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.panel2);
@@ -296,8 +296,8 @@ namespace kiosk01
             this.Controls.Add(this.home);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
